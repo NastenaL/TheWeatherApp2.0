a914173c03e3dd53a8e27a022fcf54811f013885
Anastasiia Sabodash <anastasiia.sabodash@itechcraft.com>
2021-09-19 22:07:42 +0200

    added message when cities no found

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { defaultIfEmpty, map } from 'rxjs/operators';
 import { City } from 'src/app/interfaces/city.interface';
 import { Coordinate } from 'src/app/interfaces/coordinate.interface';
 import { CityOverviewActions } from 'src/app/store/actions/city-overview.actions';
@@ -13,6 +14,11 @@ import { searchCitiesSelector } from 'src/app/store/selectors/search-cities.sele
 })
 export class SearchResultComponent {
   public readonly cities$ = this.store.select(searchCitiesSelector.selectCities);
+
+  public isEmpty$ = this.cities$.pipe(
+    map(item => item.length > 0),
+    defaultIfEmpty(false)
+  );
 
   constructor(private readonly store: Store) { }
 }
